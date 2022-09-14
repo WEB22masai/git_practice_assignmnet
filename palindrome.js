3f008e8faf135d333ed0294f01e65f377da66a2a
vedprakash kumar <vedkumar6102001@gmail.com>
2022-09-14 19:36:43 +0530

    prime and palindrome problem

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+// Check if the given string is palindrome or not
+​
+var num=121;
+var a=""+num;
+b="";
+ for(i=a.length-1;i>=0;i--){
+     b=b+a[i];
+ }if (a==b){
+   console.log(num, "is a Palindrome string");
+ }else{
+   console.log(num, "is not a Palindrome String");
+ }
---
+++ prime.js
@@ -0,0 +1,14 @@
+function identifyPrime(num) {
+    // Write code here
+    let count=0
+    for(let i=1;i<=num;i++){
+        if(num%i==0 && num>1){
+            count--;
+        }
+    }
+    if(count>2){
+        console.log("No")
+    }else{
+        console.log("Yes")
+    }
+}
